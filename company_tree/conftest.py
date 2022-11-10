6a72110e2f82dd4b657eb5513a6a735e7cfc5e2f
matyushkin <leva.matyushkin@gmail.com>
2022-11-10 22:04:37 +0300

    Init

---
+++ company_tree/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from company_tree.users.models import User
+from company_tree.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
